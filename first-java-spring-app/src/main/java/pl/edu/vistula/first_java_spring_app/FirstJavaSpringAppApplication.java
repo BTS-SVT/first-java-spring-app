3d6c0b8cb87c601d66b91349a7103b58b78105ad
AnnMaria <annmariashine978@gmail.com>
2025-05-20 22:11:45 +0200

    Initial commit

---
+++ first-java-spring-app/src/main/java/pl/edu/vistula/first_java_spring_app/FirstJavaSpringAppApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_java_spring_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstJavaSpringAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstJavaSpringAppApplication.class, args);
+	}
+
+}
---
+++ first-java-spring-app/src/test/java/pl/edu/vistula/first_java_spring_app/FirstJavaSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.first_java_spring_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstJavaSpringAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
